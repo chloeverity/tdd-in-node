30f5df94b1cc161ab1c39caf4e8305b9a82f6f19
Chloe Verity <chloeverity@hotmail.co.uk>
2019-01-16 10:39:39 +0000

    writing second failing test

--- tests/cart-summary-test.js
+++ tests/cart-summary-test.js
@@ -2,9 +2,26 @@ var chai = require("chai");
 var expect = chai.expect;
 var CartSummary = require("./../src/cart-summary");
 
-describe('CartSummary', function() {
-  it('getSubTotal() should return 0 if nothing passed in', function() {
+describe('getSubTotal() ', function() {
+  it('should return 0 if nothing passed in', function() {
     var cartSummary = new CartSummary([]);
     expect(cartSummary.getSubTotal()).to.equal(0);
+  })
+
+  it('should return the sum of the price * the quantity', function() {
+    var cartSummary = new CartSummary([{
+      id: 1,
+      quantity: 4,
+      price: 50
+    }, {
+      id: 2,
+      quantity: 2,
+      price: 30
+    }, {
+      id: 3,
+      quantity: 1,
+      price: 40
+    }])
+    expect(cartSummary.getSubTotal()).to.equal(300);
   })
 })
