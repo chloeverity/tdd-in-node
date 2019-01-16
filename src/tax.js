feca770dfc5842dac734a963e9fa6ea9e03f3c20
Chloe Verity <chloeverity@hotmail.co.uk>
2019-01-16 12:26:47 +0000

    passing test that now only charges tax (and makes a request) if you're in CA

--- src/tax.js
+++ src/tax.js
@@ -2,6 +2,9 @@ var request = require('request');
 
 module.exports = {
   calculate: function(subtotal, state, done) {
+    if (state !== 'CA') {
+      return done({ amount: 0 });
+    }
     request.post({
       url: 'https://some-tax-service.com/request',
       method: 'POST',
--- tests/tax-test.js
+++ tests/tax-test.js
@@ -33,4 +33,19 @@ describe('tax', function() {
     });
   });
 
+  it("calculate() should not make a request if the state is not CA", function(done) {
+    nock('https://some-tax-service.com')
+      .post('/request')
+      .reply(200, function(uri, requestBody) {
+        return {
+          amount: requestBody.subtotal * 0.10
+        }
+      })
+
+    tax.calculate(100, 'NY', function(taxDetails) {
+      expect(taxDetails).to.eql({ amount: 0 })
+      done();
+    })
+  })
+
 })
