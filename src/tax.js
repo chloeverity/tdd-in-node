ddf94e67afd9c0eeedcc7cf5eedfcd71b78db30e
Chloe Verity <chloeverity@hotmail.co.uk>
2019-01-16 12:22:50 +0000

    tests now fa to check if subtotal is sent in the request now passes

--- src/tax.js
+++ src/tax.js
@@ -1,5 +1,15 @@
-module.exports = {
-  calculate: function() {
+var request = require('request');
 
+module.exports = {
+  calculate: function(subtotal, state, done) {
+    request.post({
+      url: 'https://some-tax-service.com/request',
+      method: 'POST',
+      json: {
+        subtotal: subtotal
+      }
+    }, function(error, response, body) {
+      done(body)
+    });
   }
 }
