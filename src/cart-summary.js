f88a5ec110441261e6e3e755c30b49fbdab5466e
Chloe Verity <chloeverity@hotmail.co.uk>
2019-01-16 10:41:33 +0000

    updating logic to pass both tests

--- src/cart-summary.js
+++ src/cart-summary.js
@@ -1,6 +1,13 @@
-function CartSummary() {}
+function CartSummary(items) {
+  this._items = items;
+}
 
 CartSummary.prototype.getSubTotal = function() {
+  if (this._items.length) {
+    return this._items.reduce(function(subtotal, item) {
+      return subtotal += (item.quantity * item.price);
+    }, 0);
+  }
   return 0;
 }
 
